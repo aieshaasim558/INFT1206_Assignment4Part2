994a2d3abfb906adcffbaa5fd1b586d75807ed0e
aieshaasim558 <aiesha.asim@dcmail.ca>
2025-11-27 12:51:00 -0500

    added js

---
+++ Part2_ImageGallery/script.js
@@ -0,0 +1,12 @@
+/*
+Name: Aiesha Asim
+File: script.js
+Date: 28 November 2025
+Part 2 - Image Gallery
+*/
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
